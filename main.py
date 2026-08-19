204eb9137ad3086742141b3216cfb6b4de48a9a7
geoliane46-a11y <geoliane46@gmail.com>
2026-08-19 11:46:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Estou vivo!"
+    return "O bot está online!"
 
 def run():
-    port = int(os.environ.get('PORT', 1000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
@@ -18,13 +18,13 @@ def keep_alive():
     t.start()
 
 TOKEN = '8987294800:AAHvyJArQN2Bre50dJ1ilmfsD-PLGqDPm-w'
-
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "O bot está online!")
+    bot.reply_to(message, "O bot está online e funcionando!")
 
 keep_alive()
-bot.polling()
+bot.infinity_polling()
+
 
