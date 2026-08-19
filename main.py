2bd5ac33d2adfc1dd318b4df1b80ee37ff2d3c03
geoliane46-a11y <geoliane46@gmail.com>
2026-08-18 23:56:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 
 import os
 import telebot
+from flask import Flask
+from threading import Thread
+app = Flask('')
 
+@app.route('/')
+def home():
+    return "Estou vivo!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 TOKEN = '8987294800:AAHvyJArQN2Bre50dJ1ilm'
 bot = telebot.TeleBot(TOKEN)
 
@@ -9,4 +22,5 @@ bot = telebot.TeleBot(TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "O bot está online!")
 
+keep_alive()
 bot.infinity_polling()
