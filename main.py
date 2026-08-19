385a8e4526b4066244e24c570e10e74ca8c0f811
geoliane46-a11y <geoliane46@gmail.com>
2026-08-18 23:45:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+
 import os
 import telebot
 
-TOKEN = '8987294800:AAHvyJArQN2Bre50dJ1ilmfsD-PLGqDPm-w'
+TOKEN = '8987294800:AAHvyJArQN2Bre50dJ1ilm'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "O bot está online no Render!")
+    bot.reply_to(message, "O bot está online!")
 
 bot.infinity_polling()
